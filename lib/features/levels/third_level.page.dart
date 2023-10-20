21ab77da5067a2ff0c77853501fadf9cb963f196
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-10-20 09:04:40 +0200

    Add elevated buttons in third_level_page

--- lib/features/levels/third_level.page.dart
+++ lib/features/levels/third_level.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ThirdLevelPage extends StatelessWidget {
   const ThirdLevelPage({super.key});
@@ -8,7 +9,7 @@ class ThirdLevelPage extends StatelessWidget {
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/fotball2.png'),
+          image: AssetImage('assets/stadium2.png'),
           fit: BoxFit.cover,
         ),
       ),
@@ -28,6 +29,348 @@ class ThirdLevelPage extends StatelessWidget {
           backgroundColor: Colors.green,
         ),
         backgroundColor: Colors.transparent,
+        body: const Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              SissorsSlide(),
+              SizedBox(
+                height: 30,
+              ),
+              McGeadySpin(),
+              SizedBox(
+                height: 30,
+              ),
+              SlapHookTurn(),
+              SizedBox(
+                height: 30,
+              ),
+              DragSissors(),
+              SizedBox(
+                height: 30,
+              ),
+              DragStepOver(),
+              SizedBox(
+                height: 30,
+              ),
+              ElasticoStepOver(),
+              SizedBox(
+                height: 30,
+              ),
+              McGeadyUTurn(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SissorsSlide extends StatelessWidget {
+  const SissorsSlide({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+          side: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        backgroundColor: Colors.green,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 25,
+          vertical: 12,
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ThirdLevelPage(),
+          ),
+        );
+      },
+      child: Text(
+        'Sissors Slide',
+        style: GoogleFonts.robotoSlab(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class McGeadySpin extends StatelessWidget {
+  const McGeadySpin({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+          side: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        backgroundColor: Colors.green,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 25,
+          vertical: 12,
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ThirdLevelPage(),
+          ),
+        );
+      },
+      child: Text(
+        'McGeady Spin',
+        style: GoogleFonts.robotoSlab(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class SlapHookTurn extends StatelessWidget {
+  const SlapHookTurn({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+          side: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        backgroundColor: Colors.green,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 25,
+          vertical: 12,
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ThirdLevelPage(),
+          ),
+        );
+      },
+      child: Text(
+        'Slap Hook Turn',
+        style: GoogleFonts.robotoSlab(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class DragSissors extends StatelessWidget {
+  const DragSissors({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+          side: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        backgroundColor: Colors.green,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 25,
+          vertical: 12,
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ThirdLevelPage(),
+          ),
+        );
+      },
+      child: Text(
+        'Drag Sissors',
+        style: GoogleFonts.robotoSlab(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class DragStepOver extends StatelessWidget {
+  const DragStepOver({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+          side: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        backgroundColor: Colors.green,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 25,
+          vertical: 12,
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ThirdLevelPage(),
+          ),
+        );
+      },
+      child: Text(
+        'Drag Step Over',
+        style: GoogleFonts.robotoSlab(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class ElasticoStepOver extends StatelessWidget {
+  const ElasticoStepOver({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+          side: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        backgroundColor: Colors.green,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 25,
+          vertical: 12,
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ThirdLevelPage(),
+          ),
+        );
+      },
+      child: Text(
+        'Elastico Step Over',
+        style: GoogleFonts.robotoSlab(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class McGeadyUTurn extends StatelessWidget {
+  const McGeadyUTurn({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+          side: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        backgroundColor: Colors.green,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 25,
+          vertical: 12,
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ThirdLevelPage(),
+          ),
+        );
+      },
+      child: Text(
+        'McGeady U Turn',
+        style: GoogleFonts.robotoSlab(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
   }
